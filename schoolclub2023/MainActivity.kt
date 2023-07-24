134b7b00ad3dfdba598fea0206de33e64444d443
ejwl <133319140+ejwl@users.noreply.github.com>
2023-07-24 22:14:04 +0900

    base

---
+++ schoolclub2023/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.junyeong.schoolclub2023
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_main)
+    }
+}
